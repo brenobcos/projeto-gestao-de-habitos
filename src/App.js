5375d5be87fbc6add23328fb2fb26ef4b7e1e89b
Breno Carlos <bcos182@gmail.com>
2021-12-06 22:55:43 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Projeto Gestão de Hábitos</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/global.js
@@ -0,0 +1,65 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Global = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+        ::-webkit-scrollbar {
+        display: none;
+        }
+    }
+
+    ` 
+
+    /* :root {
+        --color-primary: ;
+        --color-primary-strong: ;
+        --color-secondary: ;
+        --color-secondary-light: ;
+        --color-secondary-strong: ;
+        --gray-100: ;
+        --gray-50: ;
+        --gray-20: ;
+        --gray-0: ;
+        --white: ;
+        --black: ;
+        --negative: ;
+        --warning: ;
+        --success: ;
+        --information: ;
+    }
+    body {
+        font-family: ;
+    }
+    
+    h1 {
+
+    }
+    h2 {
+
+    }
+    h3 {
+
+    }
+    span {
+
+    }
+    
+    h1, h2, h3, h4, h5, h6 {
+        font-family: ;
+    }
+    button {
+
+
+        cursor: pointer;
+    }
+    input {
+
+    }
+    a {
+
+        text-decoration: none;
+    }
+`; */
