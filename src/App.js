7dd9a68993115cf6308c972edf46473761a2d5ae
Lucas <lcssreiser@gmail.com>
2021-12-09 20:36:19 -0300

    feature: route path

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
+import Routers from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Projeto Gestão de Hábitos</h1>
-      </header>
-    </div>
+    <>
+      <Routers />
+    </>
   );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+import { Switch, Route } from "react-router-dom";
+
+const Routers = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="Register">
+        <Register />
+      </Route>
+      <Route exact path="SignIn">
+        <SignIn />
+      </Route>
+      <Route exact path="Dashboard">
+        <Dashboard />
+      </Route>
+      <Route exact path="Profile">
+        <Profile />
+      </Route>
+    </Switch>
+  );
+};
+
+export default Routers;
