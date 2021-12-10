672e0375a1a248407c55441b6a1789822913b433
Breno Carlos <bcos182@gmail.com>
2021-12-10 15:29:48 -0300

    feat: profile

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,22 @@
+import { useProfile } from "../../providers/ProfileContext";
+
+const Profile = () => {
+  const { userName, setUserName } = useProfile;
+
+  return (
+    <>
+      <button>Voltar</button>
+      <div>
+        <h1>RUN LIKE A DEV</h1>
+
+        <form>
+          <h2>Meu Perfil</h2>
+          <input type="text" placeholder="Nome" />
+          <input type="text" placeholder="Email" />
+
+          <button>Salvar</button>
+        </form>
+      </div>
+    </>
+  );
+};
