f6fcb7b1ae34b34873f0b96df143428f5b1bd2b8
arthurticianeli <arthurpeixoto@gmail.com>
2021-12-13 13:47:15 -0300

    saving for branch checkout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { Toaster } from 'react-hot-toast'
-import Login from './pages/Login';
-import './App.css'
+import { Toaster } from "react-hot-toast";
+import Login from "./pages/Login";
+
 import Routers from "./routes";
 
 function App() {
   return (
     <>
-     <Toaster position = "top-center" reverseOrder={false} />
-      <Login/>
+      <Toaster position="top-center" reverseOrder={false} />
+      <Login />
       <Routers />
     </>
-
   );
-
 }
 
-export default App
+export default App;
