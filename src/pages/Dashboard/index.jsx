a2958dea9572f7f18093ebf295c616db95306f71
Lucas <lcssreiser@gmail.com>
2021-12-15 19:29:29 -0300

    feature: groups

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -8,6 +8,7 @@ import { useState, useEffect } from "react";
 import NavigationBar from "../../components/NavigationBar";
 import Footer from "../../components/footer";
 import Goals from "../../components/ModalGoals";
+import Groups from "../../components/Groups";
 
 const Dashboard = () => {
   const token = JSON.parse(localStorage.getItem("@RunLikeaDev:token")) || "";
@@ -29,69 +30,7 @@ const Dashboard = () => {
 
   return (
     <div>
-      <NavigationBar user={user} />
-      <br></br>
-      <div>
-        <di>
-          <div>Hoje</div>
-          <div>Atividade1</div>
-          <div>Atividade2</div>
-          <div>Atividade3</div>
-          <div>Atividade4</div>
-          <div>Atividade5</div>
-          <div>Atividade6</div>
-        </di>
-        <br></br>
-        <div>
-          <div>Amanha</div>
-          <div>Atividade1</div>
-          <div>Atividade2</div>
-          <div>Atividade3</div>
-          <div>Atividade4</div>
-          <div>Atividade5</div>
-          <div>Atividade6</div>
-        </div>
-      </div>
-      <br></br>
-      <div
-        style={{
-          textAlign: "center",
-          background: "var(--black)",
-          color: "var(--white)",
-        }}
-      >
-        <div>
-          <div>Metas</div>
-          <div>Meta1</div>
-          <div>Meta2</div>
-          <div>Meta3</div>
-          <div>Meta4</div>
-          <div>Meta5</div>
-          <div>Meta6</div>
-        </div>
-        <br></br>
-        <div>
-          <div>Meus grupos</div>
-          <div>Grupo1</div>
-          <div>Grupo2</div>
-          <div>Grupo3</div>
-          <div>Grupo4</div>
-          <div>Grupo5</div>
-          <div>Grupo6</div>
-        </div>
-      </div>
-      <br></br>
-      <div>
-        <div>Meus habitos</div>
-        <div>Habito1</div>
-        <div>Habito2</div>
-        <div>Habito3</div>
-        <div>Habito4</div>
-        <div>Habito5</div>
-        <div>Habito6</div>
-      </div>
-      <br></br>
-      -------------------transformar em modais-----------
+      <Groups />
       <br></br>
       Habitos
       <Habits />
