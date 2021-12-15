f94fff013125408d92b614f924d10a60d02c3f6c
arthurticianeli <arthurpeixoto@gmail.com>
2021-12-15 10:03:48 -0300

    feat(formpadrao): criei o modal, formulario e botao pelo ant para ser utilizado em toda aplicacao

--- src/components/Inputs/index.jsx
+++ src/components/Inputs/index.jsx
@@ -2,26 +2,18 @@ import { Input } from "antd";
 
 import "antd/dist/antd.css";
 
-function Inputs({ register, name, textarea, filled, label, text }) {
+function Inputs({ name, textarea, filled, label, text }) {
   const { TextArea } = Input;
 
   return (
     <div style={{ width: "220px", marginBottom: "20px" }}>
       {label}
-      {register ? (
-        textarea ? (
-          <TextArea placeholder={text} rows={4} {...register(name)} />
-        ) : filled ? (
-          <input defaultValue={text} size="large" {...register(name)} />
-        ) : (
-          <input placeholder={text} size="large" {...register(name)} />
-        )
-      ) : textarea ? (
-        <TextArea placeholder={text} rows={4} />
+      {textarea ? (
+        <TextArea placeholder={text} rows={4} name={name} />
       ) : filled ? (
-        <Input defaultValue={text} size="large" />
+        <Input defaultValue={text} size="large" name={name} />
       ) : (
-        <Input placeholder={text} size="large" />
+        <Input placeholder={text} size="large" name={name} />
       )}
     </div>
   );
--- src/components/ModalEditarPerfil/index.jsx
+++ src/components/ModalEditarPerfil/index.jsx
@@ -6,34 +6,15 @@
 import jwt_decode from "jwt-decode";
 import api from "../../services/api";
 
-import * as yup from "yup";
-import { useForm } from "react-hook-form";
-import { toast } from "react-hot-toast";
-import { yupResolver } from "@hookform/resolvers/yup";
-
 import { useState } from "react";
-import { Button, Modal } from "antd";
-import Inputs from "../Inputs";
+import { Button, Modal, Form, Input } from "antd";
 
 function ModalEditarPerfil() {
   const token = JSON.parse(localStorage.getItem("@RunLikeaDev:token")) || "";
   const decoded = jwt_decode(token);
   const usuario = decoded.user_id;
 
-  const schema = yup.object().shape({
-    username: yup.string(),
-    email: yup.string(),
-  });
-
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-
-  const [isModalVisible, setIsModalVisible] = useState(true);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const showModal = () => {
     setIsModalVisible(true);
@@ -47,39 +28,46 @@ function ModalEditarPerfil() {
     setIsModalVisible(false);
   };
 
-  const onSubmitFunction = (data) => {
-    console.log(data);
-    api.patch(`/user/${usuario}`, data).then((response) => {
-      console.log(response);
-      toast.success("Alteração realizada com sucesso");
-    });
+  const onFinish = (data) => {
+    console.log("Success:", data);
+    // api.patch(`/user/${usuario}`, data).then((response) => {
+    //   console.log(response);
+    //   toast.success("Alteração realizada com sucesso");
+    // });
   };
 
   return (
-    <Modal
-      title="Alterar Perfil"
-      visible={isModalVisible}
-      onOk={handleOk}
-      onCancel={handleCancel}
-    >
-      <form className="form" onSubmit={handleSubmit(onSubmitFunction)}>
-        <Inputs
-          filled
-          text="Nome do cidadao"
-          label="Nome"
-          name="username"
-          register={register}
-        />
-        <Inputs
-          filled
-          text="Email do cidadao"
-          label="E-mail"
-          name="email"
-          register={register}
-        />
-        <button type="submit">Enviar</button>
-      </form>
-    </Modal>
+    <>
+      <Button onClick={showModal}>+</Button>
+      <Modal
+        title="Alterar Perfil"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Form className="form" onFinish={onFinish}>
+          <Form.Item
+            name="username"
+            label="Nome"
+            initialValue="Nome do cidadao"
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="email"
+            label="E-mail"
+            initialValue="Email do cidadao"
+          >
+            <Input type="email" />
+          </Form.Item>
+
+          <Form.Item>
+            <Button htmlType="submit">Enviar</Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
   );
 }
 
